f19c5fd1722fcce60ef3ae624b3e50301bb8ed13
GedasIncirauskas <gedas.inc@gmail.com>
2021-11-15 17:34:15 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import React from 'react';
-import { Register, Login, Navigation, Home, Add, PrivateRoute } from './components/index';
+import React, { lazy, Suspense } from 'react';
+import { Register, Login, Navigation, Home, PrivateRoute, Spinner } from './components/index';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import * as S from './App.styles';
+
+const Add = lazy(() => import('./components/Pages/Add/Add'));
 
 const App = () => {
   return (
     <S.MainContainer>
       <Router>
         <Navigation />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/add"
-            element={
-              <PrivateRoute>
-                <Add />
-              </PrivateRoute>
-            }
-          />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              exact
+              path="/add"
+              element={
+                <PrivateRoute>
+                  <Add />
+                </PrivateRoute>
+              }
+            />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </Suspense>
       </Router>
     </S.MainContainer>
   );
